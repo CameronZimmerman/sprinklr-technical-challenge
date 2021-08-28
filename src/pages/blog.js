107255724f7a73fc209b11341085b4253481266b
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-08-27 20:46:58 -0700

    create some simple linking using gatsbys optimized Link component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby'
 
 const BlogPage = () => {
   return (
     <div>
       <h1>Blog</h1>
       <p>Posts will go here!</p>
+      <p>Interested in my content, feel free to learn more <Link to="/about">about me</Link></p>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello.</h1>
-      <h2>My name is Cam, I'm a full stack engineer from Seattle who loves rock climbing.</h2>
+      <h2>My name is <Link to="/about">Cam</Link>, I'm a full stack engineer from Seattle who loves rock climbing.</h2>
+      <p>I would love to chat, please <Link to="/contact">contact me!</Link></p>
     </div>
   );
 };
