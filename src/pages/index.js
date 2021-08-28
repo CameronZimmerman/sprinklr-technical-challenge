2b5213809b11639114d65fca3637bc66fa224967
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-08-27 20:03:54 -0700

    Simple Index.js page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>My name is Cam, I'm a full stack engineer from Seattle who loves rock climbing</h2>
+    </div>
+  );
+};
+
+export default IndexPage;
