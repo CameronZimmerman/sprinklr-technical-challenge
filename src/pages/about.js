28501dfb659a9750fae316c9d0945bc5e2f5a1cf
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-08-27 20:21:32 -0700

    about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Hello! Thank you for visiting my humble blog, and for allowing me this opportunity to interview with Sprinklr. My name is Cameron Zimmerman and I am incredibly excited for the chance to meet any and all who may read this. I'm looking forward to contributing to a great team!</p>
+    </div>
+  );
+};
+
+export default AboutPage;
