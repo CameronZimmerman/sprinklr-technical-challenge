d71b4369bd6adc02787ccc71dda6e0172e4617ee
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-08-28 19:49:45 -0700

    added default styles, changed to default dart implementation of sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./header.js";
 import Footer from "./footer.js";
-import '../styles/index.css';
+import '../styles/index.scss';
 
 const Layout = ({ children }) => {
   return (
