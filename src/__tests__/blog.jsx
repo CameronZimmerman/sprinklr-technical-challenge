ee7a96c06ba3d0d9248262d593253cde75df448c
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-09-02 15:52:18 -0700

    test verbage

--- src/__tests__/blog.jsx
+++ src/__tests__/blog.jsx
@@ -3,7 +3,7 @@ import TestRenderer from "react-test-renderer";
 import BlogPage from "../pages/blog.js";
 
 describe("Blog Page", () => {
-  it("renders blog post correctly", () => {
+  it("renders blog page correctly", () => {
     const testRenderer = TestRenderer.create(<BlogPage />);
     const blogTestInstance = testRenderer.toJSON();
     expect(blogTestInstance).toMatchSnapshot();
