d054c3adee345a77ae6f88d34aa78bc536311694
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-09-01 19:47:45 -0700

    add __mocks__/file.mock.js and mock static assets in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   transform: {
     "^.+//jsx?$": `<rootDir>/jest-preprocess.js`,
   },
+  //this will mock our static assets (stylesheets with identitiy-obj-proxy, others with our file-mock.js file)
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
+  }
 };
