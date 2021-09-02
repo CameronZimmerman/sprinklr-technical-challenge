3ad71fcbd8723b870dfc1411b343c23437a3b37d
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-09-01 19:53:55 -0700

    final jest configuration, add loadershim.js file

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,14 @@ module.exports = {
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
-  }
+  },
+  //ignore any tests in node modules
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
+  //exclude gatsby module from jest transformation
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  globals: {
+    __PATH_PREFIX__: ``,
+  },
+  //files included before tests are run
+  setupFiles: [`<rootDir>/loadershim.js`],
 };
